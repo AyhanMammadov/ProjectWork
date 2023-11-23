f8e9e2509709f7853e39ffaeae7955005846cf32
Abulqasim <abulqasim.nazarov@gmail.com>
2023-11-23 22:01:34 +0400

    Create Wpf Project

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,20 @@ namespace Client
         public MainWindow()
         {
             InitializeComponent();
+
+            Cars_Loaded();
+
+        }
+
+        private async void Cars_Loaded()
+        {
+            const string address = "http://localhost:8080/cars";
+            HttpClient httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(address);
+            var responseTxt = await response.Content.ReadAsStringAsync();
+
+            
         }
     }
 }
