ceaa40f6cda6f7f42a24a567a9a039fcffc96226
Ayhan <ayhan.rmd97@gmail.com>
2023-11-23 09:55:44 -0800

    Connect SQL

---
+++ Server/Repositories/EfCoreRepository/EfRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Models;
+using Server.Repositories.Configuration;
+
+namespace Server.Repositories.EfCoreRepository;
+public class EfRepository : DbContext
+{
+    public DbSet<Car> Cars { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        optionsBuilder.UseSqlServer(connectionString: $"Server=localhost;Database=EFCoreDb;Integrated Security=True;TrustServerCertificate=True;");
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CarConfiguration());
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
